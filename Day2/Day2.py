4d9e2bb4e9bb922c38951f91164ecfa30a64deda
Gustavo Ayres Ferreira <GFerreira6@slb.com>
2021-12-06 12:30:51 -0300

    solucao parte 2 dia 2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -16,6 +16,24 @@ def CalculateVerticalPosition(rawCommands):
 def CalculatePositionResult(rawCommands):
     return CalculateHorizontalPosition(rawCommands) * CalculateVerticalPosition(rawCommands)
 
+def CalculatePositionWithAim(rawCommands):
+    aim = 0
+    depth = 0
+    horizontal = 0
+    commands = [[command.split(' ')[0],int(command.split(' ')[1])] for command in rawCommands]
+
+    for command in commands:
+        if command[0] == 'forward':
+            horizontal += command[1]
+            depth += aim * command[1]
+        elif command[0] == 'down':
+            aim += command[1]
+        elif command[0] == 'up':
+            aim -= command[1]
+    
+    return horizontal * depth
+
+
 test1CommandArray = [
 'forward 5',
 'down 5',
@@ -30,11 +48,18 @@ positionResultTest = CalculatePositionResult(test1CommandArray)
 
 print('horizontal: ' + str(horizontalPositionTest))
 print('vertical: ' + str(verticalPositionTest))
-print('vertical: ' + str(positionResultTest))
+print('position 1: ' + str(positionResultTest))
 
 
 challengeArray = ReadFromFile.ToStringArray(os.path.join(sys.path[0], 'input2.txt'))
 
 challenge1Result = CalculatePositionResult(challengeArray)
 
-print('challengeResult: ' + str(challenge1Result))
+print('challengeResult 1: ' + str(challenge1Result))
+
+test2Result = CalculatePositionWithAim(test1CommandArray)
+print('position 2: ' + str(test2Result))
+
+challenge2Result = CalculatePositionWithAim(challengeArray)
+print('challengeResult 2: ' + str(challenge2Result))
+
